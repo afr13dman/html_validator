f9749f74c95655ef0b09a78426ee3fed8f449f7a
afr13dman <akfriedman13@gmail.com>
2023-02-03 10:52:27 -0800

    created program to validate html w/o attributes

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -1,31 +1,70 @@
 #!/bin/python3
 
+import re
+
 
 def validate_html(html):
     '''
-    This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
+    This function performs a limited version of html validation
+    by checking whether every opening tag has a corresponding closing tag.
 
     >>> validate_html('<strong>example</strong>')
     True
     >>> validate_html('<strong>example')
     False
     '''
+    if len(html) == 0:
+        return True
+    html_tags = _extract_tags(html)
+    if len(html_tags) == 0:
+        return False
+    if len(html_tags) % 2 != 0:
+        return False
+    stack = []
+    for tag in html_tags:
+        if "/" not in tag:
+            stack.append(tag)
+        else:
+            if len(stack) == 0:
+                # check for closing tag
+                return False
+            else:
+                if _match(tag, stack[-1]):
+                    stack.pop()
+                else:
+                    return False
+    if len(stack) == 0:
+        return True
+    else:
+        return False
 
-    # HINT:
-    # use the _extract_tags function below to generate a list of html tags without any extra text;
-    # then process these html tags using the balanced parentheses algorithm from the class/book
-    # the main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
-    # but arbitrary text located between the html tags
+
+def _match(tag_one, tag_two):
+    '''
+    This functions checks if an opening and closing html match.
+
+    >>> _match('<strong>', '</strong>')
+    True
+    >>> _match('<strong>', '</b>')
+    False
+    '''
+    tag_one = re.findall(r'\w+', tag_one)
+    tag_two = re.findall(r'\w+', tag_two)
+    return tag_one == tag_two
 
 
 def _extract_tags(html):
     '''
     This is a helper function for `validate_html`.
-    By convention in Python, helper functions that are not meant to be used directly by the user are prefixed with an underscore.
+    By convention in Python, helper functions that are not meant
+    to be used directly by the user are prefixed with an underscore.
 
-    This function returns a list of all the html tags contained in the input string,
+    This function returns a list of all the html tags
+    contained in the input string,
     stripping out all text not contained within angle brackets.
 
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+    tags = re.findall(r'<.*?>', html)
+    return tags
